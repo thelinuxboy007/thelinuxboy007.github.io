e804448a48912ecadf4d40efb83f3eaa351a5477
Karanbir Singh <123295800+thelinuxboy007@users.noreply.github.com>
2023-02-12 15:07:44 +0000

    Add files via upload

---
+++ fashion-store/backdoor/js/commands.js
@@ -0,0 +1,72 @@
+
+var password = "mrx";
+var tryhackme = "https://tryhackme.com/p/RamghariaSaab";
+var instagram = "https://www.instagram.com/karan._ramgharia_/";
+var github = "https://github.com/karanbir-singh001/";
+var hackthebox = "https://app.hackthebox.com/profile/780921";
+
+whois = [
+  "<br>",
+  "Hii, I am Karan (THE LINUX BOY)",
+  "I like programming, CTFs, HACKING",
+  "I am interested in Purple Teaming i.e. RED TEAMING and BLUE TEAMING both.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "Bro you serious, How should I know who are you?",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'tryhackme      <a href="' + tryhackme + '" target="_blank">tryhackme/RamghariaSaab' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/karan._ramgharia_' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/karanbir-singh001' + "</a>",
+  'hackthebox     <a href="' + hackthebox + '" target="_blank">app.hackthebox.com/profile/780921' + "</a>",
+  "<br>"
+];
+
+password = [
+  "<br>",
+  '<span class="command">sudo</span>           Use only if you are an administrator',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Most projects are on GitHub or confidential..",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">sudo</span>           King of commands',
+  '<span class="command">whois</span>          Who is Karan?',
+  '<span class="command">secret</span>         To check something secret?',
+  '<span class="command">whoami</span>         who am i?',
+  '<span class="command">social</span>         It will display social media',
+  '<span class="command">password</span>       Find password',
+  '<span class="command">projects</span>       Information about projects',
+  '<span class="command">history</span>        It will display the command history',
+  '<span class="command">help</span>           I am sure you know what this command does',
+  '<span class="command">clear</span>          It will clear the terminal',
+  '<span class="command">banner</span>         It will display the header (banner)',
+  "<br>",
+];
+
+banner = [
+   "    ",
+ "          ███████╗░██████╗░█████╗░░█████╗░██╗███████╗████████╗██╗░░░██╗",
+ "          ██╔════╝██╔════╝██╔══██╗██╔══██╗██║██╔════╝╚══██╔══╝╚██╗░██╔╝",
+ "          █████╗░░╚█████╗░██║░░██║██║░░╚═╝██║█████╗░░░░░██║░░░░╚████╔╝░",
+ "          ██╔══╝░░░╚═══██╗██║░░██║██║░░██╗██║██╔══╝░░░░░██║░░░░░╚██╔╝░░",
+ "          ██║░░░░░██████╔╝╚█████╔╝╚█████╔╝██║███████╗░░░██║░░░░░░██║░░░",
+ "          ╚═╝░░░░░╚═════╝░░╚════╝░░╚════╝░╚═╝╚══════╝░░░╚═╝░░░░░░╚═╝░░░",
+ " ",
+ "                                 𝗞𝗔𝗥𝗔𝗡: 𝗧𝗛𝗘 𝗟𝗜𝗡𝗨𝗫 𝗕𝗢𝗬",
+ "------------------------------------------------------------------------------------- ",
+  '<span class="color2">WELCOME TO THE OFFICIAL BACKDOOR OF WEBSITE. I HOPE YOU WILL ENJOY IT. PLEASE DO NOT USE THIS BACKDOOR FOR ILLEGAL ACTIVITIES :)</span>',
+  "<span class=\"color2\">For List of Commands Please Type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
